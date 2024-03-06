756fcadf7d390e554339ba3d0121cc072ba5151a
Ben Matt <benjaminmatthys@gmail.com>
2024-03-06 18:24:29 +1100

    re added project, and moved connection to API to a more appropriate component.

---
+++ RecipeSite/src/app/recipe-list/recipe-list.component.ts
@@ -0,0 +1,47 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+interface RecipeListItem {
+  id: string;
+  title: string;
+
+}
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrl: './recipe-list.component.css'
+})
+export class RecipeListComponent implements OnInit {
+  public recipes: RecipeListItem[] = [];
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.getRecipeList();
+  }
+
+  getRecipeList() {
+    this.http.get<RecipeListItem[]>('https://localhost:7087/MasterRecipeList').subscribe
+      (
+
+        (result) => {
+          console.log(result);
+
+
+          this.recipes = result;
+
+
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
+
+
+  }
+
+
+
+  title = 'RecipieSite';
+}
