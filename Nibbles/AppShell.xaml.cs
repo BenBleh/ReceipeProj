bbd7b45fa6f51472240747c4a4d8358386ccb002
Benjamin Matthys <benjaminmatthys@gmail.com>
2026-05-26 12:40:48 +1000

    close enough nav bar fix

--- Nibbles/AppShell.xaml.cs
+++ Nibbles/AppShell.xaml.cs
@@ -10,6 +10,35 @@ namespace Nibbles
             // Register routes for Shell navigation
             Routing.RegisterRoute(nameof(RecipeDetailsPage), typeof(RecipeDetailsPage));
             Routing.RegisterRoute(nameof(AddEditPage), typeof(AddEditPage));
+#if ANDROID
+            this.Navigated += Shell_Navigated;
+#endif
+            // Apply shell colors now and when system theme changes
+            ApplyShellTheme(Application.Current.RequestedTheme);
+            Application.Current.RequestedThemeChanged += (s, e) => ApplyShellTheme(e.RequestedTheme);
+        }
+
+#if ANDROID
+        void Shell_Navigated(object sender, ShellNavigatedEventArgs e)
+        {
+            MainActivity.Current?.RefreshToolbarColor();
+        }
+#endif
+
+        void ApplyShellTheme(AppTheme theme)
+        {
+            if (theme == AppTheme.Dark)
+            {
+                this.BackgroundColor = (Microsoft.Maui.Graphics.Color)Application.Current.Resources["OffBlack"];
+                this.SetValue(Shell.ForegroundColorProperty, (Microsoft.Maui.Graphics.Color)Application.Current.Resources["White"]);
+                this.SetValue(Shell.TitleColorProperty, (Microsoft.Maui.Graphics.Color)Application.Current.Resources["White"]);
+            }
+            else
+            {
+                this.BackgroundColor = (Microsoft.Maui.Graphics.Color)Application.Current.Resources["White"];
+                this.SetValue(Shell.ForegroundColorProperty, (Microsoft.Maui.Graphics.Color)Application.Current.Resources["Black"]);
+                this.SetValue(Shell.TitleColorProperty, (Microsoft.Maui.Graphics.Color)Application.Current.Resources["Black"]);
+            }
         }
     }
 }
--- Nibbles/Platforms/Android/MainActivity.cs
+++ Nibbles/Platforms/Android/MainActivity.cs
@@ -1,11 +1,74 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using AndroidX.Core.Content;
 
 namespace Nibbles
 {
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        public static MainActivity? Current { get; private set; }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            Current = this;
+            base.OnCreate(savedInstanceState);
+        }
+
+        protected override void OnDestroy()
+        {
+            if (Current == this)
+            {
+                Current = null;
+            }
+            base.OnDestroy();
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            RefreshToolbarColor();
+        }
+
+        public void RefreshToolbarColor()
+        {
+            try
+            {
+                var root = Window?.DecorView;
+                if (root != null)
+                {
+                    ApplyToolbarColor(root);
+                }
+            }
+            catch { }
+        }
+
+        void ApplyToolbarColor(Android.Views.View view)
+        {
+            if (view is AndroidX.AppCompat.Widget.Toolbar toolbar)
+            {
+                int colorInt;
+                try
+                {
+                    colorInt = ContextCompat.GetColor(this, Resource.Color.colorPrimary);
+                }
+                catch
+                {
+                    colorInt = ContextCompat.GetColor(this, global::Android.Resource.Color.Black);
+                }
+
+                toolbar.SetBackgroundColor(new Android.Graphics.Color(colorInt));
+                toolbar.Elevation = 0f;
+            }
+
+            if (view is Android.Views.ViewGroup group)
+            {
+                for (int i = 0; i < group.ChildCount; i++)
+                {
+                    ApplyToolbarColor(group.GetChildAt(i));
+                }
+            }
+        }
     }
 }
